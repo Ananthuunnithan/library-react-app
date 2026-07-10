c6c82df0d11090446bc73f76c301483103e48740
Ananthu Unnithan <unnithanananthu69@gmail.com>
2026-07-10 11:26:06 +0530

    dynamic

--- src/components/ViewBooks.jsx
+++ src/components/ViewBooks.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const ViewBooks = () => {
+    const [data, changeData] = useState(
+        [
+            { "title": "The Great Gatsby", "avatar": "https://covers.openlibrary.org/b/isbn/9780743273565-L.jpg", "description": "Available on Amazon" },
+            { "title": "To Kill a Mockingbird", "avatar": "https://covers.openlibrary.org/b/isbn/9780061120084-L.jpg", "description": "Available on Amazon" },
+            { "title": "1984", "avatar": "https://covers.openlibrary.org/b/isbn/9780451524935-L.jpg", "description": "Available on Amazon" },
+            { "title": "The Alchemist", "avatar": "https://covers.openlibrary.org/b/isbn/9780062315007-L.jpg", "description": "Available on Amazon" },
+            { "title": "Harry Potter and the Philosopher's Stone", "avatar": "https://covers.openlibrary.org/b/isbn/9780747532743-L.jpg", "description": "Available on Amazon" },
+        ]
+    )
     return (
         <div>
             <NavBar />
@@ -10,126 +19,27 @@ const ViewBooks = () => {
                     <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                         <div class="row g-3">
-                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            {data.map(
+                                (value, index) => {
+                                    return (
 
-                                <div class="card">
-                                    <img src="https://covers.openlibrary.org/b/isbn/9780743273565-L.jpg" class="card-img-top" alt="..." height="350px" />
-                                    <div class="card-body">
-                                        <h5 class="card-title">The Great Gatsby</h5>
-                                        <p class="card-text">Available on Amazon</p>
-                                        <a href="#" class="btn btn-primary">Buy now</a>
-                                    </div>
-                                </div>
+                                        <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
-                            </div>
-                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                            <div class="card">
+                                                <img src={value.avatar} class="card-img-top" alt="..." height="350px" />
+                                                <div class="card-body">
+                                                    <h5 class="card-title">{value.title}</h5>
+                                                    <p class="card-text">{value.description}</p>
+                                                    <a href="#" class="btn btn-primary">Buy now</a>
+                                                </div>
+                                            </div>
 
-                                <div class="card">
-                                    <img src="https://covers.openlibrary.org/b/isbn/9780061120084-L.jpg" class="card-img-top" alt="..." height="350px" />
-                                    <div class="card-body">
-                                        <h5 class="card-title">To Kill a Mockingbird</h5>
-                                        <p class="card-text">Available on Amazon</p>
-                                        <a href="#" class="btn btn-primary">Buy now</a>
-                                    </div>
-                                </div>
+                                        </div>
 
-                            </div>
-                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                    )
+                                }
+                            )}
 
-                                <div class="card">
-                                    <img src="https://covers.openlibrary.org/b/isbn/9780451524935-L.jpg" class="card-img-top" alt="..." height="350px" />
-                                    <div class="card-body">
-                                        <h5 class="card-title">1984</h5>
-                                        <p class="card-text">Available on Amazon</p>
-                                        <a href="#" class="btn btn-primary">Buy now</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div class="card">
-                                    <img src="https://covers.openlibrary.org/b/isbn/9780062315007-L.jpg" class="card-img-top" alt="..." height="350px" />
-                                    <div class="card-body">
-                                        <h5 class="card-title">The Alchemist</h5>
-                                        <p class="card-text">Available on Amazon</p>
-                                        <a href="#" class="btn btn-primary">Buy now</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div class="card">
-                                    <img src="https://covers.openlibrary.org/b/isbn/9780747532743-L.jpg" class="card-img-top" alt="..." height="350px" />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Harry Potter and the Philosopher's Stone</h5>
-                                        <p class="card-text">Available on Amazon</p>
-                                        <a href="#" class="btn btn-primary">Buy now</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div class="card">
-                                    <img src="https://covers.openlibrary.org/b/isbn/9780547928227-L.jpg" class="card-img-top" alt="..." height="350px" />
-                                    <div class="card-body">
-                                        <h5 class="card-title">The Hobbit</h5>
-                                        <p class="card-text">Available on Amazon</p>
-                                        <a href="#" class="btn btn-primary">Buy now</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div class="card">
-                                    <img src="https://covers.openlibrary.org/b/isbn/9780140449136-L.jpg" class="card-img-top" alt="..." height="350px" />
-                                    <div class="card-body">
-                                        <h5 class="card-title">The Odyssey</h5>
-                                        <p class="card-text">Available on Amazon</p>
-                                        <a href="#" class="btn btn-primary">Buy now</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div class="card">
-                                    <img src="https://covers.openlibrary.org/b/isbn/9780307277671-L.jpg" class="card-img-top" alt="..." height="350px" />
-                                    <div class="card-body">
-                                        <h5 class="card-title">The Kite Runner</h5>
-                                        <p class="card-text">Available on Amazon</p>
-                                        <a href="#" class="btn btn-primary">Buy now</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div class="card">
-                                    <img src="https://covers.openlibrary.org/b/isbn/9780385472579-L.jpg" class="card-img-top" alt="..." height="350px" />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Zen and the Art of Motorcycle Maintenance</h5>
-                                        <p class="card-text">Available on Amazon</p>
-                                        <a href="#" class="btn btn-primary">Buy now</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div class="card">
-                                    <img src="https://covers.openlibrary.org/b/isbn/9781501128035-L.jpg" class="card-img-top" alt="..." height="350px" />
-                                    <div class="card-body">
-                                        <h5 class="card-title">It Ends With Us</h5>
-                                        <p class="card-text">Available on Amazon</p>
-                                        <a href="#" class="btn btn-primary">Buy now</a>
-                                    </div>
-                                </div>
-
-                            </div>
                         </div>
 
                     </div>
